ccb988a94bd1c1a053b25ee03752f26fcdbc5619
James Weatherhead <jweatherhead@outlook.com>
2018-04-03 00:28:17 -0700

    ready for submission

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -26,10 +26,10 @@ UKF::UKF() {
   P_ = MatrixXd(5, 5);
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 30;
+  std_a_ = 2;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 30;
+  std_yawdd_ = .5;
   
   //DO NOT MODIFY measurement noise values below these are provided by the sensor manufacturer.
   // Laser measurement noise standard deviation position1 in m
@@ -56,8 +56,6 @@ UKF::UKF() {
   Hint: one or more values initialized above might be wildly off...
   */
 
-  std_a_ = 2;
-  std_yawdd_ = 1;
 
   n_x_ = 5;
   n_aug_ = 7; // add in the process noise for angle and angle rate
